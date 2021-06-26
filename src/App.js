3fc85c383a6e1647007917eb81d63a5814ca9903
ENTOMDT <entomdt@gmail.com>
2021-06-26 15:55:00 -0500

    Add stack holders

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import React, { useContext } from 'react';
+
+const GameContext = React.createContext();
 
 function App() {
   return (
@@ -13,12 +16,33 @@ function App() {
           <Col>Discard Stead</Col>
         </Row>
         <Row>
-          <Col>Stead 1</Col>
-          <Col>Stead 2</Col> 
-          <Col>Stead 3</Col>
-          <Col>Stead 4</Col>
+          <Col>
+            <Stack />
+          </Col>
+          <Col>
+            <Stack />
+          </Col>
+        </Row>
+        <Row> 
+          <Col>
+            <Stack />
+          </Col>
+          <Col>
+            <Stack />
+          </Col>
         </Row>
       </Container> 
+    </div>
+  );
+}
+
+function Stack() {
+  const jpgPath = "https://raw.githubusercontent.com/BBuchholz/make-ten-mod/jpg-mod/public/images/cards-jpg/2D.jpg";
+  const cardKey = "2D";
+  return (
+    <div>
+      <img src={jpgPath}
+           alt={"cardKey:" + cardKey} /> 
     </div>
   );
 }

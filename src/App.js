95ec3a898f52114febf593b25e93e28a04bbca1c
ENTOMDT <entomdt@gmail.com>
2021-06-26 15:00:09 -0500

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div class="tableau">
-      <div class="stead stacker">Stead One</div>
-      <div class="stead stacker">Stead Two</div>
-      <div class="stead stacker">Stead Three</div>
-      <div class="stead stacker">Stead Four</div>
-      <div class="stead">Draw Pile</div>
-      <div class="stead">Discard Pile</div>
+    <div className="App">
+      <Container className="tableau" fluid>
+        <Row>
+          <Col>Draw Stead</Col>
+          <Col>Discard Stead</Col>
+        </Row>
+        <Row>
+          <Col>Stead 1</Col>
+          <Col>Stead 2</Col> 
+          <Col>Stead 3</Col>
+          <Col>Stead 4</Col>
+        </Row>
+      </Container> 
     </div>
   );
 }
